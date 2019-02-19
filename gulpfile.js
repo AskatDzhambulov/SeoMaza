6673442ce76ceda16167544da5dda11b76d6b58b
AskatDzhambulov <askat.dzhambulov@iaau.edu.kg>
2019-02-19 20:16:07 +0600

    Added initial gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,29 @@
-function defaultTask(cb) {
-  // place code for your default task here
-  cb();
+const { series, src, dest, parallel } = require('gulp');
+const sass = require('gulp-sass');
+var cleanHandle = require('gulp-clean');
+sass.compiler = require('node-sass');
+
+const clean = () => {
+    return src('dist', {read: false})
+    .pipe(cleanHandle());
 }
 
-exports.default = defaultTask
+function css() {
+  return src('./sass/**/*.scss')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(dest('./dist/css'));  
+}
+
+function html () {
+  return src('./templates/**/*.html')
+  .pipe(dest('./dist/'))
+}
+
+function js(){
+  return src('./js/**/*.js')
+  .pipe(dest('./dist/js'))
+}
+
+exports.css = css;
+exports.clean = clean;
+exports.default = series(clean, parallel(html, css, js));
