9055349edd381f886788b1aecc73aea50e52a08b
AskatDzhambulov <askat.dzhambulov@iaau.edu.kg>
2019-02-23 19:42:34 +0600

    added browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,35 @@
 const { series, src, dest, parallel, watch } = require('gulp');
 const sass = require('gulp-sass');
+const browsersync = require("browser-sync").create();
 sass.compiler = require('node-sass');
 const googleWebFonts = require('gulp-google-webfonts');
 const del = require('del'); 
 // var plumber = require('gulp-plumber');
 
 
+
+const browserSync = (done) => {
+  browsersync.init({
+    server: {
+      baseDir: "./dist/"
+    },
+    port: 3000,
+  });
+  done();
+}
+
+const browserSyncReload = (done) => {
+  browsersync.reload();
+  done();
+}
+
 const clean = () => del(['dist']);
 
 const css = () => {
   return src(['./sass/**/*.scss'])
   .pipe(sass({includePaths: require('node-normalize-scss').includePaths}).on('error', sass.logError))
-  .pipe(dest('./dist/css'));  
+  .pipe(dest('./dist/css'))
+  .pipe(browsersync.stream());
 }
 
 const html = () => {
@@ -51,12 +69,14 @@ const images = () => src('./images/**/*').pipe(dest('./dist/images'));
 const build = series(clean, parallel(html, images, fontawesome, fonts, css, js));
 
 const dev = () => {
-  watch('./templates/**/*.html', html);
-  watch('./images/**/*', images)
-  watch('./sass/**/*.scss', css);
+  watch('./templates/**/*.html', html),
+  watch('./images/**/*', images),
+  watch('./sass/**/*.scss', css),
+  watch('./dist', browserSyncReload);
+
 }
 
-const runDev = series(build, dev)
+const runDev = series(build, parallel(dev,browserSync));
 
 exports.css = css;
 exports.clean = clean;
